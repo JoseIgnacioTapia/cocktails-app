82957b538764743851e209bf37d4aef4ce09bafd
JoeIt <josignaciot@gmail.com>
2023-05-03 18:31:44 -0300

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.svg';
 
 function Navbar(): JSX.Element {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo" className="logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
