f111a9e231829ee3da4d9057a92851a4a9222b5b
JoeIt <josignaciot@gmail.com>
2023-05-06 22:08:49 -0300

    searching cocktails

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,6 +9,8 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+export type AppDispatch = typeof store.dispatch;
+
 export const store = configureStore({
   reducer: rootReducer,
 });
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,30 @@
 import { useEffect, useRef, FormEvent } from 'react';
+import { useDispatch } from 'react-redux';
+// import { unwrapResult } from '@reduxjs/toolkit';
+import { AppDispatch } from '../app/store';
+import { searchByTerm } from '../features/cocktails/cocktailtSlice';
 
 function SearchForm(): JSX.Element {
   const searchValue = useRef<HTMLInputElement>(null);
 
+  const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
     searchValue.current?.focus();
   }, []);
+
+  const searchHandler = async () => {
+    const searchTerm = searchValue.current?.value || '';
+    try {
+      // const actionResult = await dispatch(searchByTerm(searchTerm));
+      // const searchResult = unwrapResult(actionResult);
+
+      // console.log(searchResult);
+      dispatch(searchByTerm(searchTerm));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,7 +35,12 @@ function SearchForm(): JSX.Element {
       <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search yourfavorite cocktail</label>
-          <input type="text" id="name" ref={searchValue} />
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchHandler}
+          />
         </div>
       </form>
     </section>
